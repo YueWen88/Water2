5af017ad855c6ca29381ea6ec0107ecef964c117
YueWen88 <2536840270@qq.com>
2024-10-13 22:02:09 +0800

    添加Layers游戏层级

--- Water/src/Hazel/Application.h
+++ Water/src/Hazel/Application.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include "Core.h"
-#include "Events/Event.h"
-#include "Hazel/Events/ApplicationEvent.h"
 
 #include "Window.h"
+#include "LayerStack.h"
+#include "Events/Event.h"
+#include "Hazel/Events/ApplicationEvent.h"
 
 namespace Hazel {
 	class HAZEL_API Application
@@ -16,11 +17,15 @@ namespace Hazel {
 		void Run();
 
 		void OnEvent(Event& e);
+
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* layer);
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
+		LayerStack m_LayerStack;
 	};
 	// To be defined in CLIENT
 	Application* CreateApplication();
